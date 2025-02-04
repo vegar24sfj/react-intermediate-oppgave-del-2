a269cd0e82000feb4c5367e85252fa5b4923fa74
vegar24sfj <vegar24sfj@gmail.com>
2025-02-04 21:13:31 +0100

    fifth commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="flex justify-between items-center p-4">
       <div className="flex items-center space-x-4">
-        <img src="/public/assets/logo.webp" alt="Logo" className="h-10" />
+        <img src="/assets/logo.webp" alt="Logo" className="h-10" />
         <h1 className="text-2xl">My Portfolio</h1>
       </div>
       <NavBar />
